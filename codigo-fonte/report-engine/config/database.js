7137b0361f7ba3c089611ffdf4ff3117c6069906
Jonathan Cortoppassi <jonathan-516@hotmail.com>
2025-05-18 12:30:55 -0300

    feat: database

--- codigo-fonte/report-engine/config/database.js
+++ codigo-fonte/report-engine/config/database.js
@@ -1,15 +1,20 @@
-// config/database.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'loja',        // Nome do banco de dados
-  process.env.DB_USER || 'admin',       // Usuário
-  process.env.DB_PASSWORD || 'admin',   // Senha
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost', // Host do banco (vai usar 'db' no Docker)
-    port: process.env.DB_PORT || 3306,        // Porta do MySQL
-    dialect: 'mysql',                         // Dialeto do banco
-    logging: false,                           // Desativa logs no console
+    // host: process.env.DB_HOST || 'localhost', // Host do banco (vai usar 'db' no Docker)
+    // port: process.env.DB_PORT || 3306,        // Porta do MySQL
+    // dialect: 'mysql',                         // Dialeto do banco
+    // logging: false,                           // Desativa logs no console
+
+    host: '127.0.0.1', // ou 'localhost'
+    port: 3306,
+    dialect: 'mysql',
+    logging: false, // opcional
   }
 );
 
